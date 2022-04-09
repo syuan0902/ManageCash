477cf8bb07a32d17bf8642d0e49ee0d4509905e6
syuan7492 <kurosaki7492@gmail.com>
2022-04-10 04:18:10 +0800

    Add project files.

---
+++ ManageCash/Guy.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageCash
+{
+    class Guy
+    {
+        public string m_name;
+        public int m_cash;
+
+        public void WriteMyInfo()
+        {
+            Console.WriteLine(m_name + "有" + m_cash + "元。");
+        }
+
+        public int GiveCash(int amount)
+        {
+            if(amount <= 0)
+            {
+                Console.WriteLine(m_name + "說：" + amount + "元不是有效金額。");
+                return 0;
+            }
+
+            if(amount > m_cash)
+            {
+                Console.WriteLine(m_name + "說：我沒有足夠的現金支付" + amount + "元。");
+                return 0;
+            }
+
+            m_cash -= amount;
+
+            return amount;
+        }
+
+        public void ReceiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine(m_name + "說：我沒有收到錢。");
+            }
+
+            else
+            {
+                m_cash += amount;
+            }
+        }
+    }
+}
---
+++ ManageCash/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ManageCash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guy Kyo = new Guy() { m_cash = 50, m_name = "Kyo" };
+            Guy Syuan = new Guy() { m_cash = 100, m_name = "Syuan" };
+
+            while(true)
+            {
+                Kyo.WriteMyInfo();
+                Syuan.WriteMyInfo();
+
+                Console.Write("請輸入一個金額：　" );
+                string m_howMuch = Console.ReadLine();
+                if (m_howMuch == "") return;
+                
+                if(int.TryParse(m_howMuch, out int amount))
+                {
+                    Console.Write("誰應該給錢： ");
+                    string m_whichGuy = Console.ReadLine();
+                    
+                    if(m_whichGuy == "Kyo")
+                    {
+                        int m_cashGiven = Kyo.GiveCash(amount);
+                        Syuan.ReceiveCash(m_cashGiven);
+                    }
+
+                    else if(m_whichGuy == "Syuan")
+                    {
+                        int m_cashGiven = Syuan.GiveCash(amount);
+                        Kyo.ReceiveCash(m_cashGiven);
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("請輸入'Kyo'或是'Syuan'");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("請輸入一個金額(或按Enter離開)");
+                }
+            }
+           
+        }
+    }
+}
